156b806ff3c6a3392a79da38b9c97a443fe90c49
Yury Kotov <bairkan@gmail.com>
2015-01-26 20:54:47 +0400

    Added other js dependencies

--- app/assets/javascripts/langtrainer_frontend_backbone.js
+++ app/assets/javascripts/langtrainer_frontend_backbone.js
@@ -2,7 +2,12 @@
 //= require underscore
 //= require underscore.string
 //= require backbone
-//= require sticky.full.js
+//= require sticky.full
+//= require jquery.cookie
+//= require jquery.insert_at_caret
+//= require unicode-base
+//= require unicode-categories
+//= require xregexp
 //= require ./langtrainer_frontend_backbone/application
 
 if (!window.Langtrainer) {
