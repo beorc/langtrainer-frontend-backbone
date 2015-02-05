814e27358930a8f790bd2e1bb471efccfce6379f
Yury Kotov <bairkan@gmail.com>
2015-02-05 20:51:02 +0400

    Removed close.png

--- vendor/assets/javascripts/sticky.full.js
+++ vendor/assets/javascripts/sticky.full.js
@@ -57,7 +57,7 @@
 			{
 			// Building and inserting sticky note
 			$(this).find('.sticky-queue').prepend('<div class="sticky border-' + position + '" id="' + uniqID + '"></div>');
-			$(this).find('#' + uniqID).append('<img src="/assets/close.png" class="sticky-close" rel="' + uniqID + '" title="Close" />');
+			$(this).find('#' + uniqID).append('<img src="close.png" class="sticky-close" rel="' + uniqID + '" title="Close" />');
 			$(this).find('#' + uniqID).append('<div class="sticky-note" rel="' + uniqID + '">' + note + '</div>');
 
 			// Smoother animation
